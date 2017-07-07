e5537373766dadc753e3adeb7cba392bc2d2439e
lisga <lsg@rrtimes.com>
2017-07-07 18:21:55 +0800

    生成dubbo代理对象的接口

--- remote/dubbo/index.js
+++ remote/dubbo/index.js
@@ -1,6 +1,7 @@
 const IAppService = require('./IAppService.js')
 const ILoginService = require('./ILoginService.js')
 
+//http://localhost:8086/v1/user/ping?java2js=1&itf=com.rrtimes.user.itf.ILoginService
 module.exports = {
   IAppService,
   ILoginService,
